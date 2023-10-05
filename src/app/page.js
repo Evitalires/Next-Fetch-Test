0c757f26f5c7ad92db3c57814e1b951cc6e810ae
evitalires <evitalires@gmail.com>
2023-10-05 13:24:43 -0500

    adding card styles and internationalization api

--- src/app/page.js
+++ src/app/page.js
@@ -1,55 +1,71 @@
 "use client";
 import { useEffect } from "react";
-const baseUrl = "https://platzi-avo.vercel.app";
+const BASE_URL = "https://platzi-avo.vercel.app";
 
-console.log("Happy codding");
-
-//web api
+const formatPrice = (price) => {
+  const newPrice = new window.Intl.NumberFormat("en-En", {
+    style: "currency",
+    currency: "USD",
+  }).format(price);
+  return newPrice;
+};
 
 export default function Home() {
   useEffect(() => {
-    const main = document.getElementsByTagName("main")[0];
     const aguacates = document.querySelector("#aguacates");
 
     // window is accessible here.
     window
-      .fetch(`${baseUrl}/api/avo`)
+      .fetch(`${BASE_URL}/api/avo`)
       .then((res) => res.json())
       .then((responseJSON) => {
         const allItems = [];
-        responseJSON.data.forEach((item) => {
-          // crear imagen
-          const imagen = document.createElement("img");
-          imagen.src = `${baseUrl}${item.image}`;
+        responseJSON.data.forEach((aguacate) => {
+          const image = document.createElement("img");
+          image.className =
+            "h-16 w-16 md:h-24 md:w-24 rounded-full mx-auto md:mx-0 md:mr-6";
+          image.src = `${BASE_URL}${aguacate.image}`;
 
-          // crear titulo
+          // Create heading
+          // <h2 class="text-lg">Erin Lindford</h2>
           const title = document.createElement("h2");
-          title.textContent = item.name;
-          title.className = "text-xl";
+          title.className = "text-gray-600 text-lg";
+          title.textContent = aguacate.name;
 
-          // crear precio
+          // Create Price
+          // <div class="text-gray-600">(555) 765-4321</div>
           const price = document.createElement("div");
-          price.textContent = item.price;
+          price.className = "text-gray-600";
+          price.textContent = formatPrice(aguacate.price);
+
+          // Wrap price & title
+          // <div class="text-center md:text-left"><price ><title ></div>
+          const priceAndTitle = document.createElement("div");
+          priceAndTitle.className = "text-center md:text-left";
+          priceAndTitle.appendChild(title);
+          priceAndTitle.appendChild(price);
 
           //aguacate item
-          const aguacate = document.createElement("div");
-          aguacate.append(imagen, title, price);
-          aguacate.className = "w-80 p-4 m-4";
+          // Wrap Img and priceAndTitle
+          // <div class="md:flex bg-white rounded-lg p-6">
+          const card = document.createElement("div");
+          card.className =
+            "md:flex bg-white rounded-lg p-6 m-4 hover:bg-gray-300";
+          card.appendChild(image);
+          card.appendChild(priceAndTitle);
 
           //push
-          allItems.push(aguacate);
+          allItems.push(card);
         });
         aguacates.append(...allItems);
       });
-  });
+  }, []);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-14">
-      <h1 className="text-5xl border-red-400 border-8 p-4"> Aguacates</h1>
-      <div
-        id="aguacates"
-        className="m-8 w-full border-yellow-400 border-8 p-4 grid grid-cols-2"
-      ></div>
+      <h1 className="text-5xl p-4"> Aguacates Store</h1>
+
+      <div id="aguacates" className="m-8 w-full p-4 grid grid-cols-2"></div>
     </main>
   );
 }
